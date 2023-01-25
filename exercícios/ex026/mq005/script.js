af99be5a25104e1f53f296d451d0359702dbb54b
Matheus Colombo <111023158+mathcolombo@users.noreply.github.com>
2023-01-25 19:10:34 -0300

    .

---
+++ exercícios/ex026/mq005/script.js
@@ -0,0 +1,21 @@
+function espTamanho() {
+    
+    if (window.innerWidth >= 768) {
+
+        itens.style.display = 'block'
+    }
+}
+
+function clickMenu() {
+
+    if (itens.style.display == 'block') {
+
+        itens.style.display = 'none'
+
+    } else {
+
+        itens.style.display = 'block'
+
+    }
+
+}
